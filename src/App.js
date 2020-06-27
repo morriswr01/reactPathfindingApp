d74a054bf222219e738f3de13b6570b577be2f62
morriswr01 <morriswr01@gmail.com>
2020-06-27 14:43:39 +0100

    Basic create-react-app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// CSS
+import "reset-css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
